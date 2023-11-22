679d28a5e5a21ec6ceea0d59281eca7373969840
Nabidbhai12 <1905013@ugrad.cse.buet.ac.bd>
2023-11-23 00:16:22 +0600

    installing all and creating template

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function App() {
+  return <h1 className='text-red-500'>Hello,i am nabid and i am not going to accept defeat.I will come back inshallah</h1>
+}
